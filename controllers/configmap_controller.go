30ab468da4c13faa5208e3d1b25f8bc4b34e0724
Andy Goldstein <andy.goldstein@redhat.com>
2022-05-25 16:16:37 -0400

    Expand CRUD testing
    Signed-off-by: Andy Goldstein <andy.goldstein@redhat.com>

--- controllers/configmap_controller.go
+++ controllers/configmap_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	kcpclient "github.com/kcp-dev/apimachinery/pkg/client"
 	corev1 "k8s.io/api/core/v1"
@@ -35,13 +36,47 @@ func (r *ConfigMapReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	ctx = kcpclient.WithCluster(ctx, req.ObjectKey.Cluster)
 
-	var configmap corev1.ConfigMap
-	if err := r.Get(ctx, req.ObjectKey, &configmap); err != nil {
+	// Test get
+	var configMap corev1.ConfigMap
+
+	if err := r.Get(ctx, req.ObjectKey, &configMap); err != nil {
 		log.Error(err, "unable to get configmap")
-		return ctrl.Result{}, err
+		return ctrl.Result{}, nil
 	}
 
-	log.Info("Retrieved configmap")
+	log.Info("Get: retrieved configMap")
+
+	// Test list
+	var configMapList corev1.ConfigMapList
+	if err := r.List(ctx, &configMapList); err != nil {
+		log.Error(err, "unable to list configmaps")
+		return ctrl.Result{}, nil
+	}
+	for _, cm := range configMapList.Items {
+		log.Info("List: got", "namespace", cm.Namespace, "name", cm.Name)
+	}
+
+	labels := configMap.Labels
+	if labels == nil {
+		return ctrl.Result{}, nil
+	}
+
+	// Test Update
+	if labels["name"] == "" {
+		return ctrl.Result{}, nil
+	}
+
+	response := fmt.Sprintf("hello-%s", labels["name"])
+
+	if labels["response"] == response {
+		return ctrl.Result{}, nil
+	}
+
+	labels["response"] = response
+
+	if err := r.Update(ctx, &configMap); err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
